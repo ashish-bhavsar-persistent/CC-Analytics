354930b83c530a7fc7152974e710f81f4cc8090d
Subhadip Jot <jotsubhadip@gmail.com>
2019-06-11 13:37:09 +0530

    Login Page with Token retrive

--- FrontEnd-CC-Analytics/src/app/services/auth.service.ts
+++ FrontEnd-CC-Analytics/src/app/services/auth.service.ts
@@ -1,44 +1,40 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { map } from "rxjs/operators";
+import { environment } from "../../environments/environment";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class AuthService {
+  // private readonly JWT_TOKEN = 'JWT_TOKEN';
+  // private readonly REFREH_TOKEN = 'REFREH_TOKEN';
+  // private loggedUser: string;
 
-    // private readonly JWT_TOKEN = 'JWT_TOKEN';
-    // private readonly REFREH_TOKEN = 'REFREH_TOKEN';
-    // private loggedUser: string;
+  private loginUrl = environment.ENV.baseURL + "/login";
+  private tokenUrl = environment.ENV.baseURL + "/oauth/token";
 
-    private loginUrl = environment.ENV.baseURL + "/login";
-    private tokenUrl = environment.ENV.baseURL + "/oauth/token";
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient){}
+  getToken(username, password): Observable<any> {
+    const body = new HttpParams()
+      .set("username", username)
+      .set("password", password)
+      .set("grant_type", "password");
 
-    
+    let headers = new HttpHeaders({
+      "Content-type": "application/x-www-form-urlencoded; charset=utf-8",
+      Authorization: "Basic " + btoa("ashish:secret")
+    });
+    //let options = new HttpRequest({ headers: headers });
+    console.log(body.toString());
+    console.log(headers);
 
-    getToken(username, password):Observable<any>{
-
-        const body = new HttpParams()
-        .set('username', username)
-        .set('password', password)
-        .set('grant_type','password');
-
-        let headers = new HttpHeaders();
-        headers.set('Content-Type', 'application/x-www-form-urlencoded');
-        headers.set('Authorization', 'Basic YXNoaXNoOnNlY3JldA==');
-
-        console.log(body.toString());
-        console.log(headers)
-
-        return this.http.post(this.tokenUrl, body.toString(), {
-            withCredentials: true,
-            headers:{ 'Authorization' : 'Basic YXNoaXNoOnNlY3JldA=='}
-        } )
-        .pipe(map (res => {
-            return res;
-        }))
-    }
-
+    return this.http
+      .post(this.tokenUrl, body.toString(), { headers: headers })
+      .pipe(
+        map(res => {
+          return res;
+        })
+      );
+  }
 }
--- FrontEnd-CC-Analytics/src/environments/environment.prod.ts
+++ FrontEnd-CC-Analytics/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  ENV : {
+    baseURL : 'http://localhost:8080'
+  }
 };
