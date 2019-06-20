2d5ffa60be5ee4d5ec3d213e8b809a2db937f1dc
Shree <shree@Shrees-iMac.local>
2019-06-20 23:38:34 +0530

    Autocomplete done.

--- FrontEnd-CC-Analytics/src/app/components/autocomplete-input/autocomplete-input.component.ts
+++ FrontEnd-CC-Analytics/src/app/components/autocomplete-input/autocomplete-input.component.ts
@@ -1,58 +1,67 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { startWith, map } from 'rxjs/operators';
+import { map, startWith } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 
 export interface User {
-  accountId: string;
   accountName: string;
+  accountId: string;
 }
 
+/**
+ * @title Display value autocomplete
+ */
 @Component({
   selector: 'autocomplete-input',
-  templateUrl: './autocomplete-input.component.html',
-  styleUrls: ['./autocomplete-input.component.scss']
+  templateUrl: 'autocomplete-input.component.html',
+  styleUrls: ['autocomplete-input.component.scss'],
 })
 export class AutocompleteInputComponent implements OnInit {
-
-  @Input() placeholderData : string;
-  constructor(private http: HttpClient) { }
+  @Input() placeholderData: string;
   myControl = new FormControl();
   options: User[] = [];
   filteredOptions: Observable<User[]>;
 
-  ngOnInit() {
+  constructor(private http: HttpClient) {
 
+  }
+
+  ngOnInit() {
     let headers = new HttpHeaders({
       Authorization: 'Bearer ' + `${sessionStorage.getItem("token")}`
     });
 
-    this.http.get(environment.ENV.baseURL+'/api/v1/accounts/name', {headers:headers})
-    .subscribe(res => {
-      let temp:any = res;
-      console.log(temp);
-      for(let i of temp){
-        this.options.push(i);
-      }
-    })
+    this.http.get(environment.ENV.baseURL + '/api/v1/accounts/name', { headers: headers })
+      .subscribe(res => {
+        let temp:any = res;
+        for (let i of temp) {
+          this.options.push(i);
+        }
+        this.filteredOptions = this.myControl.valueChanges
+          .pipe(
+            startWith(''),
+            // map(value => typeof value === 'string' ? value : value.name),
+            // map(name => name ? this._filter(name) : this.options.slice())
+            map(value => this._filter(value))
+          );
+      })
 
-    this.filteredOptions = this.myControl.valueChanges
-      .pipe(
-        startWith(''),
-        map(value => typeof value === 'string' ? value : value.name),
-        map(name => name ? this._filter(name) : this.options.slice())
-      );
+
   }
+
+
 
   displayFn(user?: User): string | undefined {
     return user ? user.accountName : undefined;
   }
 
   private _filter(name: string): User[] {
-    const filterValue = name.toLowerCase();
+    // const filterValue = name.toLowerCase();
+    // return this.options.filter(option => option.accountName.toLowerCase().indexOf(filterValue) === 0);
 
-    return this.options.filter(option => option.accountName.toLowerCase().indexOf(filterValue) === 0);
+    const filterValue = name.toLowerCase();
+    return this.options.filter(option => option.accountName.toLowerCase().includes(filterValue));
   }
 }
